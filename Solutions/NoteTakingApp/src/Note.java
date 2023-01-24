3aea185611d961a2d1afdc38da077e390765f7a1
Hrishi2520 <hrishikeshshedge0@gmail.com>
2023-01-24 17:28:58 +0530

    NoteTakingApp

---
+++ Solutions/NoteTakingApp/src/Note.java
@@ -0,0 +1,32 @@
+public class Note {
+    private String title;
+    private String description;
+
+    public Note (String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Note: [" +
+                "title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ']';
+    }
+}
